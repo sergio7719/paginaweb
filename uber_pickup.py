bcaa529dd9cf8d11c66854c0653d667bf80b323f
Sergio Berlanga Alvarez <sergio.berlanga@ids.com.mx>
2024-02-07 14:42:16 -0600

    se agrega archivo de la aplicacion streamlit con un histograma

--- uber_pickup.py
+++ uber_pickup.py
@@ -21,7 +21,7 @@ def load_data(nrows):
 # Se crea una etiqueta de cargando info
 data_load_state = st.text('Loading data...')
 
-#Aqui se cargan solo 1000 registros de todo el archivo
+#Aqui se cargan solo 10000 registros de todo el archivo
 
 data = load_data(10000)
 
@@ -29,9 +29,20 @@ data = load_data(10000)
 
 data_load_state.text("Done! (using st.cache_data)")
 
+
 # Se carga la informacion del dt y solo se muestasn las 10 primerfas filas
-st.subheader("Raw data")
-st.write(data.head(10))
+
+#st.subheader("Raw data")
+#st.write(data.head(10))
+
+
+# Se utilizara un boton para poder seleccionar la info con la funcion st.checkbox
+
+if st.checkbox("Show raw data"):
+    st.subheader("Raw data")
+    st.write(data.head(10))
+
+
 
 #se agrega subtitulo
 
@@ -42,4 +53,23 @@ st.subheader("Number of pickups by hour")
 hist_values = np.histogram(
     data[DATE_COLUMN].dt.hour, bins=24, range= (0,24)) [0]
 
+#este metodo es navitvo de streamlit y sirve para dibujar histogrmas
 st.bar_chart(hist_values)
+
+
+#Trazar datos en un mapa
+
+st.subheader("Map of all pickups")
+
+#st.map() sirve superponer datos en un mapa
+
+#st.map(data)
+
+#hour_to_filter = 17
+
+#Slider sirve para poner un control dislisante interactivo
+
+hour_to_filter = st.slider("hour", 0, 23, 17) # 0h, max: 23h, defualt 17 (hora filtrada)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+st.subheader(f"Map of all pickups at {hour_to_filter}:00")
+st.map(filtered_data)
